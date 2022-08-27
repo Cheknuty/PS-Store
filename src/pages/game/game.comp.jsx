a43cffdf215e83cf93620c91d669461db1103549
zero <azizovxikmatjon04@gmail.com>
2022-08-27 13:08:14 +0500

    alfa

--- src/pages/game/game.comp.jsx
+++ src/pages/game/game.comp.jsx
@@ -1,15 +1,7 @@
-import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
-import { TinyItem } from "../../atoms/tinyItem/tinyItem.comp"
-import { TinyItemSkeleton } from "../../atoms/tinyItemSkeleton/tinyItemSkeleton.comp"
 import { EpicSection } from "../../molecules/epicEection/epicSection.comp"
 import { GameWrapper } from "./game.style"
 
 export function Game() {
-    const { id } = useParams()
-    const allGames = useSelector(state => state.allGamesList.games)
-    const status = useSelector(state => state.allGamesList.status)
-    const game = allGames.find(game => game.id === id)
     return (
         <GameWrapper>
             <EpicSection />
